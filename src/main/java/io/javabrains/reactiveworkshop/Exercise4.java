0166930901306646a842bcd7bd3fa30f37dcea37
Lim Siew Leng <siewleng.lim@gmail.com>
2023-12-23 16:32:11 +0800

    Exercise4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -3,17 +3,28 @@ package io.javabrains.reactiveworkshop;
 import java.io.IOException;
 
 public class Exercise4 {
+    // Use ReactiveSources.intNumberMono()
+
+    // Print the value from intNumberMono when it emits
+    // TODO: Write code here
+    public void function1 () {
+        System.out.println("Print the value from intNumberMono when it emits");
+        ReactiveSources.intNumberMono()
+        .subscribe(System.out::println);
+    }
+    
+    
+    // Get the value from the Mono into an integer variable
+    // TODO: Write code here
+    public void function2 () {
+        System.out.println("Get the value from the Mono into an integer variable");
+        Integer num = ReactiveSources.intNumberMono().block();
+        System.out.println(num);
+    }
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono()
-
-        // Print the value from intNumberMono when it emits
-        // TODO: Write code here
-
-        // Get the value from the Mono into an integer variable
-        // TODO: Write code here
-
+        new Exercise4().function2();
         System.out.println("Press a key to end");
         System.in.read();
     }
