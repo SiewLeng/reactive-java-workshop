2d25084c99d276845f7f6aa4bd2a1a2915280a5e
Lim Siew Leng <siewleng.lim@gmail.com>
2023-12-23 16:37:15 +0800

    edit Exercise2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -4,23 +4,28 @@ import java.io.IOException;
 
 public class Exercise2 {
 
-    public static void main(String[] args) throws IOException {
+    // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
-        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
-
-        // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+    // Print all numbers in the ReactiveSources.intNumbersFlux stream
+    // TODO: Write code here
+    public void function1() {
         System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
         ReactiveSources.intNumbersFlux().subscribe(System.out::println);
+    }
 
-        // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+    // Print all users in the ReactiveSources.userFlux stream
+    // TODO: Write code here
+    public void function2() {
         System.out.println("Print all users in the ReactiveSources.userFlux stream");
         ReactiveSources.userFlux()
         .map(user -> user.toString())
         .doOnNext(System.out::println)
         .subscribe();
+    }
 
+    public static void main(String[] args) throws IOException {
+
+        new Exercise2().function2();
         System.out.println("Press a key to end");
         System.in.read();
     }
