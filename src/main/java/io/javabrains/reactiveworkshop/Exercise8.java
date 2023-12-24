75a1205761e5f7f8d9217d819a10a0def5995cbf
Lim Siew Leng <siewleng.lim@gmail.com>
2023-12-24 21:47:24 +0800

    exericse 8

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -1,26 +1,39 @@
 package io.javabrains.reactiveworkshop;
 
+import java.io.IOException;
 import reactor.core.publisher.Flux;
 
-import java.io.IOException;
-
 public class Exercise8 {
+    
+    // Use ReactiveSources.intNumbersFluxWithException()
 
+    // Print values from intNumbersFluxWithException and continue on errors
+    // TODO: Write code here
+    public void function1() {
+        ReactiveSources.intNumbersFluxWithException()
+        .doOnError(err -> System.out.println("An error has happended"))
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage())
+        );
+    }
+
+    // Print values from intNumbersFluxWithException and when errors
+    // happen, replace with a fallback sequence of -1 and -2
+    // TODO: Write code here
+    public void function2() {
+        ReactiveSources.intNumbersFluxWithException()
+        // .onErrorContinue((e, item) -> System.out.println(e.getMessage() + " " + item))
+        .onErrorResume(err -> Flux.just(-1, -2))
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage())
+        );
+    }
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumbersFluxWithException()
-
-        // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
-
-        // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
-
-        // Print values from intNumbersFluxWithException and when errors
-        // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
-
+        new Exercise8().function2();
         System.out.println("Press a key to end");
         System.in.read();
     }
