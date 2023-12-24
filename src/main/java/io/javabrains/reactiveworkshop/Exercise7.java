22967ce31f23892bfe2dd567b110afd163a5aa7f
Lim Siew Leng <siewleng.lim@gmail.com>
2023-12-24 18:31:39 +0800

    exercise7

--- src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
@@ -4,32 +4,104 @@ import java.io.IOException;
 
 public class Exercise7 {
 
+    // Print all values from intNumbersFlux that's greater than 5
+    // TODO: Write code here
+    public void function1() {
+        System.out.println("Print all values from intNumbersFlux that's greater than 5");
+        ReactiveSources.intNumbersFlux()
+        .filter(x -> x > 5)
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
+
+    // Print 10 times each value from intNumbersFlux that's greater than 5
+    // TODO: Write code here
+    public void function2() {
+        System.out.println("Print 10 times each value from intNumbersFlux that's greater than 5");
+        ReactiveSources.intNumbersFlux()
+        .filter(x -> x > 5)
+        .map(x -> x * 10)
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
+
+    // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
+    // TODO: Write code here
+    public void function3() {
+        System.out.println("Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5");
+        ReactiveSources.intNumbersFlux()
+        .filter(x -> x > 5)
+        .take(3)
+        .map(x -> x * 10)
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
+
+    // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
+    // TODO: Write code here
+    public void function4() {
+        System.out.println("Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found");
+        ReactiveSources.intNumbersFlux()
+        .filter(x -> x > 20)
+        .defaultIfEmpty(-1)
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
+
+    // Switch ints from intNumbersFlux to the right user from userFlux
+    // TODO: Write code here
+    public void function5() {
+        System.out.println("Switch ints from intNumbersFlux to the right user from userFlux");
+        ReactiveSources.intNumbersFlux()
+        .flatMap(x -> ReactiveSources.userFlux().filter(user -> user.getId() == x))
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
+
+    // Print only distinct numbers from intNumbersFluxWithRepeat
+    // TODO: Write code here
+    public void function6() {
+        System.out.println("Print only distinct numbers from intNumbersFluxWithRepeat");
+        ReactiveSources.intNumbersFluxWithRepeat()
+        .distinct()
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
+
+    // Print from intNumbersFluxWithRepeat excluding immediately repeating numbers
+    // TODO: Write code here
+     public void function7() {
+        System.out.println("Print from intNumbersFluxWithRepeat excluding immediately repeating numbers");
+        ReactiveSources.intNumbersFluxWithRepeat()
+        .distinctUntilChanged()
+        .subscribe(
+            System.out::println,
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("Complete")
+        );
+    }
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
-
-        // Print all values from intNumbersFlux that's greater than 5
-        // TODO: Write code here
-
-        // Print 10 times each value from intNumbersFlux that's greater than 5
-        // TODO: Write code here
-
-        // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
-        // TODO: Write code here
-
-        // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
-        // TODO: Write code here
-
-        // Switch ints from intNumbersFlux to the right user from userFlux
-        // TODO: Write code here
-
-        // Print only distinct numbers from intNumbersFluxWithRepeat
-        // TODO: Write code here
-
-        // Print from intNumbersFluxWithRepeat excluding immediately repeating numbers
-        // TODO: Write code here
-
+       new Exercise7().function7();
         System.out.println("Press a key to end");
         System.in.read();
     }
