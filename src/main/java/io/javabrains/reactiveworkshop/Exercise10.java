cd11a1e36bf68491838d0db908d5541c84d51e80
Lim Siew Leng <siewleng.lim@dxc.com>
2024-03-14 20:08:42 +0800

    edit Exercise10

--- src/main/java/io/javabrains/reactiveworkshop/Exercise10.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise10.java
@@ -3,16 +3,31 @@ import java.io.IOException;
 
 import reactor.core.publisher.Flux;
 
+
 public class Exercise10 {
 
-    public Flux<Object> function1() {
+    public Flux<User> function1() {
         // error is thrown
-        return ReactiveSources.intNumbersFluxWithException().map(x -> null).onErrorResume(err -> Flux.error(err));
+        return ReactiveSources.userFlux()
+        .map(x -> {
+            if (x.getId() > 0) {
+                return null;
+            }
+            return x;
+        })
+        .onErrorResume(err -> Flux.error(err));
     }
 
     public static void main(String[] args) throws IOException {
         // the error is handled and not thrown
-        new Exercise10().function1().onErrorResume(err -> Flux.just(-1, -2)).subscribe(System.out::println);
+        new Exercise10().function1()
+            .map(x -> new User(x.getId(), x.getFirstName() + " edit", x.getLastName()))
+            .onErrorResume(err -> {
+                System.out.println("err: " + err);
+                return Flux.empty();
+            })
+            .subscribe(System.out::println);
+
         System.out.println("Press a key to end");
         System.in.read();
     }
