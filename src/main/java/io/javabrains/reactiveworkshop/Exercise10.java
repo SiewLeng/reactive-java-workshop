cc186d357a538a608dc687fb337f15d5584f09f3
Lim Siew Leng <siewleng.lim@dxc.com>
2024-03-14 22:31:57 +0800

    edit Exercise10

--- src/main/java/io/javabrains/reactiveworkshop/Exercise10.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise10.java
@@ -6,11 +6,11 @@ import reactor.core.publisher.Flux;
 
 public class Exercise10 {
 
-    public Flux<User> function1() {
+    public Flux<User> getUser() {
         // error is thrown
         return ReactiveSources.userFlux()
         .map(x -> {
-            if (x.getId() > 0) {
+            if (x.getId() > 2) {
                 return null;
             }
             return x;
@@ -18,18 +18,35 @@ public class Exercise10 {
         .onErrorResume(err -> Flux.error(err));
     }
 
-    public static void main(String[] args) throws IOException {
+    public void function1() {
         // the error is handled and not thrown
-        new Exercise10().function1()
-            .map(x -> new User(x.getId(), x.getFirstName() + " edit", x.getLastName()))
-            .onErrorResume(err -> {
-                System.out.println("err: " + err);
-                return Flux.empty();
-            })
-            .subscribe(System.out::println);
+        this.getUser()
+        .map(x -> new User(x.getId(), x.getFirstName() + " edit", x.getLastName()))
+        .onErrorResume(err -> {
+            System.out.println("err: " + err);
+            return Flux.empty();
+        })
+        .subscribe(System.out::println);
+    }
 
+    public void function2() {
+        // the error thrown is not caught in the Catch block;
+        try {
+            this.getUser()
+            .map(x -> new User(x.getId(), x.getFirstName() + " edit", x.getLastName()))
+            .subscribe(System.out::println);
+        } catch (Exception e) {
+            System.out.println("An exception error occurs!");
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+      
+        new Exercise10().function2();
         System.out.println("Press a key to end");
         System.in.read();
+    
     }
 
 }
